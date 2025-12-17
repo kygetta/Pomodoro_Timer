0afff4eb9a1bc6541ed60242e1ee609efe5d0582
Kylie Hall <kyliehall354@gmail.com>
2025-12-16 21:08:54 -0500

    Updated timer & music logic

--- mainPomo.py
+++ mainPomo.py
@@ -1,32 +1,62 @@
 from datetime import datetime
 import time
 
-from strudel import *
-
 # Print time start & finish
 current_time = datetime.now()
 print(current_time)
 
 # Active (on) timer vs break (15) in minutes
 # After 4 pomodoros, take a longer break 15-30 minutes
-pomOn = int(25 * 60) # Seconds
-pomOff = int(15 * 60) # Seconds
 
-def music():
-    note("<[c3 e3 g3 b3]>").sound("piano").room(0.8)
+#pomOn = int(25 * 60) # Seconds
+#pomOff = int(15 * 60) # Seconds
 
+class Pomodoro:
+    def __init__(self):
+        self.break_time = int(5) # seconds
+        self.work_time = int(10) # seconds
+        self.pom_done = 0
 
-def countdown(t):
+    def PomWork(self):
+        while self.pom_done < 2:
 
-    while t > 0:
-        minutes, remaining_seconds = divmod(t, 60)
-        time_format = '{:02d}:{:02d}'.format(minutes, remaining_seconds)
+            # WORK TIME
+            print("\n POMODORO START!!")
+            self.timer_logic(self.work_time)
 
-        print(time_format)
-        t -= 1 # Countdown #
-        time.sleep(1)
-    print("Break Time!!")
+            #Increment state counter
+            self.pom_done += 1
+
+            if self.pom_done == 2:
+                # Work Complete
+                print("\n Well Done! The work is complete!")
+                break
+
+            # BREAK TIME
+            print("\n Break time!")
+
+            print("\n Enjoy the break.. you deserve it!")
+            self.timer_logic(self.break_time)
+
+    def PomBreak(self):
+        while self.work_time >= 0:
+            pomo.timer_logic(self.work_time)
+            if self.work_time == 0:
+
+                # Break's up, back to work
+                print("\n Time to get back to work. You've got this!")
+
+            timer -= 1
+
+    def timer_logic(self, t):
+        while t > 0:
+            minutes, remaining_seconds = divmod(t, 60)
+            time_format = '{:02d}:{:02d}'.format(minutes, remaining_seconds)
+            print(time_format)
+            t -= 1 # Countdown #
+            time.sleep(1)
 
 #function call
-#countdown(int(pomOn))
-music()
+if __name__ == '__main__':
+    pomo = Pomodoro()
+    pomo.PomWork()
